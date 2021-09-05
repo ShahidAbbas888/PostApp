63406238a2658aa678eb0d1367e62a7a9da5ffcc
Shahid Abbas <53262340+ShahidAbbas888@users.noreply.github.com>
2021-09-05 17:13:12 +0100

    Update db.js

--- backend/db/db.js
+++ backend/db/db.js
@@ -6,12 +6,9 @@ const mongoose = require('mongoose')
 
 
 
-//const url =   "mongodb+srv://shahid:bsef16m028@cluster0.bt06e.mongodb.net/PostApp?retryWrites=true&w=majority";
-//`mongodb+srv://sample_user:<password>@my-sample-cluster-b3ugy.mongodb.net/<dbname>?retryWrites=true&w=majority`;
-const url="mongodb://shahid:shahid@127.0.0.1:27017/MyPostApp";
-//const url="mongodb://"+env.DB_USER+env.DB_PASSWORD+"@127.0.0.1:27017/"+env.DB_NAME;
 
-//const url = `mongodb://${env.DB_USER}:${env.DB_PWD}@${env.DB_HOST}:${env.DB_PORT}/${env.DB_DBNAME}`;
+
+const url = `mongodb://${env.DB_USER}:${env.DB_PWD}@${env.DB_HOST}:${env.DB_PORT}/${env.DB_DBNAME}`;
 
 const connectionParams={
     useNewUrlParser: true,
